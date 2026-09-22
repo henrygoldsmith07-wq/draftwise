d9eefc47172fb1d8881e533539a30a137b7ad149
henrygoldsmith07-wq <henrygoldsmith07@gmail.com>
2026-09-22 17:45:29 +0100

    Match generated TypeScript bundle formatting

--- extension/shared-provider.js
+++ extension/shared-provider.js
@@ -567,7 +567,7 @@ function preserveCase(original, replacement) {
     return replacement;
 }
 function issueTextFingerprint(text) {
-    let hash = 2166136261;
+    let hash = 2_166_136_261;
     for (let index = 0; index < text.length; index += 1) {
         hash ^= text.charCodeAt(index);
         hash = Math.imul(hash, 16777619);
